7f7886617f36435aef6857b05d8170e5e494c96d
Poul Foged Nielsen <poul@poulfoged.com>
2020-08-19 14:49:17 +0200

    update to 6+

--- build/push-new-version.bat
+++ build/push-new-version.bat
@@ -1,4 +1,4 @@
 @echo off
 del *.nupkg
 tools\nuget pack ..\source\NestClientFactory\NestClientFactory.csproj
-tools\nuget push *.nupkg
+tools\nuget push *.nupkg -Source https://api.nuget.org/v3/index.json
